f7b0d938b44106b785ef259360603619270cf5cb
UjjwalTyagi15e <ujjwaltyagi1512@gmail.com>
2023-03-14 22:53:45 +0530

    new change to merge array

--- Array-Merge_2_arrays.cpp
+++ Array-Merge_2_arrays.cpp
@@ -1,24 +1,31 @@
-// we make a gap variable =ceil((float) m+n/2)
-// we concatinate the 2 arrays as they are so that we only have to deal with one array that is a1
-// we run a loop while(gap>0)
-// in this loop we traverse the a1 array from 2 positions 1 is from the start(i=0) and other is from the gap position (i.e if the gap is 4 then j =4). if we find that a1[i]>a1[j] then we swap them and increment both i and j by 1 ;
-// after j goes out of bounds we reset gap to be half of what it was earlier unless its 1 then its set to 0 .
-// eg if gap starts with 4 then becomes 2 then 1 and then 0 
+// take 3 pointer . pointing to end of 1st array(i),end of second array(j),end of merged array which is basically to be accomodated in 1st array(k)
+// now we start comparing from the end and whichever is greater in value gets placed in k and its index gets decremented . keep doing it untill i or j reaches 0 
+// if by chance any element in arr 2 are left then simply add them behind the arr1 as they are as all the comparisons are made already 
+
+
 
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i=0,gap=ceil((float)(m+n)/2);
-        for(int i=0;i<n;i++){
-            nums1[m+i]=nums2[i];
+        int i=m-1;
+        int j=n-1;
+        int k=m+n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i]>=nums2[j]){
+                nums1[k]=nums1[i];
+                k--;
+                i--;
+            }
+            else {
+                nums1[k]=nums2[j];
+                k--;
+                j--;
+            }
         }
-        while(gap>0){
-            for(int i=0,j=gap;j<m+n;j++,i++){
-                if(nums1[i]>nums1[j])swap(nums1[i],nums1[j]);
-            }    
-            if(gap==1)gap=0;
-            else gap=ceil((float)gap/2);
+        while(j>=0){
+            nums1[k--]=nums2[j--];
         }
-        
+
+        return ;
     }
 };
